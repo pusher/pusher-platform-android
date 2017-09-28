b2145c4c34d44a3a3d3a5caaa9391c10462ff223
Zan Markan <zan.markan@gmail.com>
2017-09-28 16:21:34 +0100

    Start implementing token providing state machine

--- library/src/main/kotlin/com/pusher/platform/subscription/TokenProvidingSubscription.kt
+++ library/src/main/kotlin/com/pusher/platform/subscription/TokenProvidingSubscription.kt
@@ -1,8 +1,9 @@
 package com.pusher.platform.subscription
 
+import com.pusher.platform.SubscriptionListeners
 import com.pusher.platform.logger.Logger
 import com.pusher.platform.tokenProvider.TokenProvider
-import elements.Subscription
+import elements.*
 
 
 fun createTokenProvidingStrategy(tokenProvider: TokenProvider? = null, tokenParams: Any? =  null, logger: Logger, nextSubscribeStrategy: SubscribeStrategy): SubscribeStrategy {
@@ -18,7 +19,7 @@ fun createTokenProvidingStrategy(tokenProvider: TokenProvider? = null, tokenPara
 
 }
 
-class TokenProvidingSubscription: Subscription {
+class TokenProvidingSubscription(listeners: SubscriptionListeners, val headers: Headers, val tokenProvider: TokenProvider, val tokenParams: Any? = null, val logger: Logger, val nextSubscribeStrategy: SubscribeStrategy): Subscription {
 
     var state: SubscriptionState
 
@@ -31,15 +32,84 @@ class TokenProvidingSubscription: Subscription {
     }
 
     init {
-        state = TokenProvidingState()
+        state = TokenProvidingState(listeners, onTransition)
     }
 
-    inner class TokenProvidingState : SubscriptionState {
+    inner class TokenProvidingState(val listeners: SubscriptionListeners, onTransition: StateTransition) : SubscriptionState {
+
+        lateinit var underlyingSubscription: Subscription
+        init {
+            logger.verbose("${TokenProvidingSubscription@this}: Transitioning to TokenProvidingState")
+            fetchTokenAndExecuteSubscription()
+        }
 
         override fun unsubscribe() {
+            TODO()
+        }
 
 
+        fun fetchTokenAndExecuteSubscription() {
+            tokenProvider.fetchToken(
+                    tokenParams = tokenParams,
+                    onSuccess = { token ->
+                        headers.insertToken(token)
+                        logger.verbose("${TokenProvidingSubscription@this}: token fetched: $token")
+                        underlyingSubscription = nextSubscribeStrategy(
+                                SubscriptionListeners(
+                                        onOpen = { headers -> onTransition(OpenSubscriptionState(listeners, headers, underlyingSubscription, onTransition))},
+                                        onRetrying = listeners.onRetrying,
+                                        onError = {
+                                            error ->
+                                                if(error.tokenExpired()){
+                                                    tokenProvider.clearToken(token)
+                                                    fetchTokenAndExecuteSubscription()
+                                                }
+                                                else{
+                                                    onTransition(FailedSubscriptionState(error))
+                                                }
+                                        },
+                                        onEvent = listeners.onEvent,
+                                        onSubscribe = listeners.onSubscribe,
+                                        onEnd = {
+                                            error -> onTransition(EndedSubscriptionState(error))
+                                        }
+                                ),
+                                headers
+                        )
+
+
+                    },
+                    onFailure = { error -> onTransition(FailedSubscriptionState(error)) }
+            )
+        }
+    }
+
+    inner class OpenSubscriptionState(listeners: SubscriptionListeners, headers: Headers, subscription: Subscription, onTransition: StateTransition): SubscriptionState {
+        override fun unsubscribe() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
 
     }
+
+    inner class FailedSubscriptionState(error: Error): SubscriptionState {
+        override fun unsubscribe() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+    inner class EndedSubscriptionState(error: EOSEvent?): SubscriptionState {
+        override fun unsubscribe() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+}
+
+private fun Headers.insertToken(token: String) {
+    this.put("Authorization", listOf("Bearer $token"))
+}
+
+private fun elements.Error.tokenExpired(): Boolean {
+    return this is ErrorResponse && this.statusCode == 401 && this.error == "authentication/expired"
 }
