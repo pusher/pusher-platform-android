66489a08c67207e47247bef557e3f5f97b5f2608
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-02-21 18:03:23 +0000

    Add command to install firebase component

--- gradle/firebase/install.sh
+++ gradle/firebase/install.sh
@@ -6,3 +6,4 @@ gcloud version || true
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
+gcloud install firebase
