76020864bdc753b4aaeb997b971520eaef7f783a
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-02-22 10:26:11 +0000

    No manual installation of firebase

--- gradle/firebase/install.sh
+++ gradle/firebase/install.sh
@@ -7,4 +7,3 @@ if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; ex
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
 gcloud --quiet components update
-gcloud --quiet components install firebase
