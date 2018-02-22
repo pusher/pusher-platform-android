26ac3ac2486c7226bbe11f1053a3dce6c4481e00
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-02-22 13:57:31 +0000

    Extra logs on firebase installation

--- gradle/firebase/install.sh
+++ gradle/firebase/install.sh
@@ -2,9 +2,20 @@
 
 echo "Running ${BASH_SOURCE[0]}"
 
+echo "Version before install:"
 gcloud version || true
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+  echo "Installing gcloud"
+  rm -rf $HOME/google-cloud-sdk;
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
+  curl https://sdk.cloud.google.com | bash;
+else
+  echo "Found 'gcloud' is present"
+fi
 source $HOME/google-cloud-sdk/path.bash.inc
+echo "Version after install"
 gcloud version
+echo "Update gcloud components"
 gcloud --quiet components update
+echo "List components: "
 gcloud components list
