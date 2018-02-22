1e07d3506da967b006445707a7058fa995245e71
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-02-22 09:52:35 +0000

    Update also needs to be quiet

--- gradle/firebase/install.sh
+++ gradle/firebase/install.sh
@@ -6,5 +6,5 @@ gcloud version || true
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
-gcloud components update
+gcloud --quiet components update
 gcloud --quiet components install firebase
