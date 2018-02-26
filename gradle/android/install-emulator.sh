0acc71e8c6ac4bbed933c094ef7e814339259aca
pablisco <pabs87@gmail.com>
2018-02-26 16:09:59 +0100

    Adds list of available emulators

--- gradle/android/install-emulator.sh
+++ gradle/android/install-emulator.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+android list targets
 echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
