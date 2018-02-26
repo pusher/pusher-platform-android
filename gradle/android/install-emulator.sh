9760359473cce1c7801bfe44cacf9a4e402ff0f8
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-02-26 15:34:51 +0000

    Adds specific id for device emulator creation

--- gradle/android/install-emulator.sh
+++ gradle/android/install-emulator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 android list targets
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+echo no | android create avd --force -n test -k "system-images;android-27;google_apis;x86"
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
