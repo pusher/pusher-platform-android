6d8bf5a53c5a0115c3a93fc235b22cedc449189f
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-03-13 14:44:14 +0000

    Oops, wrong version

--- library-core/src/main/kotlin/elements/events.kt
+++ library-core/src/main/kotlin/elements/events.kt
@@ -11,7 +11,7 @@ sealed class SubscriptionMessage {
             return when (message[0].asInt) {
                 0 -> ControlEvent(body = message[1].asString)
                 1 -> SubscriptionEvent(eventId = message[1].asString, headers = message[2].parseOr { emptyMap<String, List<String>>() }, body = message[3])
-                255 -> EOSEvent(statusCode = message[1].asInt, headers = message[2] as Headers, errorBody = message[3])
+                255 -> EOSEvent(statusCode = message[1].asInt, headers = message[2].parseOr { emptyMap<String, List<String>>() }, errorBody = message[3])
                 else -> throw kotlin.Error("Unknown message type: $messageString") // TODO: Handle more gracefully
             }
         }
