d77ed22572af4c2b3c7c13fed3e18715e235b650
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-10 18:30:32 +0100

    Flaky integration tests

--- library-core/src/integrationTest/kotlin/com/pusher/platform/InstanceIntegrationSpek.kt
+++ library-core/src/integrationTest/kotlin/com/pusher/platform/InstanceIntegrationSpek.kt
@@ -1,10 +1,15 @@
 package com.pusher.platform
 
+import com.google.common.truth.Truth.assertThat
 import com.pusher.SdkInfo
 import com.pusher.platform.logger.Logger
 import com.pusher.platform.network.ConnectivityHelper
 import com.pusher.platform.retrying.RetryStrategyOptions
 import com.pusher.platform.test.*
+import elements.ErrorResponse
+import elements.Subscription
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.launch
 import mockitox.stub
 import org.jetbrains.spek.api.Spek
 
@@ -29,10 +34,24 @@ class InstanceIntegrationSpek : Spek({
             instance.subscribeNonResuming(
                 path = PATH_10_AND_EOS,
                 retryOptions = RetryStrategyOptions(limit = 0),
-                listeners = observeUntil(
-                    open = 1,
-                    events = 10,
-                    end = 1
+                listeners = listenersWithCounter(
+                    onEvent = { events++ },
+                    onEnd = { done { assertThat(events).isEqualTo(10) } },
+                    onError = { fail("We should not get an error") }
+                )
+            )
+        }
+
+        will("subscribe, terminate on EOS, and trigger onEnd callback exactly once") {
+            instance.subscribeNonResuming(
+                path = PATH_3_AND_OPEN,
+                retryOptions = RetryStrategyOptions(limit = 0),
+                listeners = listenersWithCounter(
+                    onEnd = {
+                        end++
+                        done { assertThat(end).isEqualTo(1) }
+                    },
+                    onError = { fail("We should not get an error") }
                 )
             )
         }
@@ -41,30 +60,60 @@ class InstanceIntegrationSpek : Spek({
             instance.subscribeNonResuming(
                 path = PATH_3_AND_OPEN,
                 retryOptions = RetryStrategyOptions(limit = 0),
-                listeners = observeUntil(
-                    open = 1,
-                    events = 3
+                listeners = listenersWithCounter(
+                    onEvent = {
+                        events++
+                        attempt { assertThat(events).isLessThan(4) }
+                        if (events == 3) done()
+                    },
+                    onEnd = { fail("onEnd triggered. This shouldn't be!") },
+                    onError = { fail("onError triggered - this shouldn't be!") }
                 )
             )
         }
 
-        will("subscribes and receives EOS immediately - expecting onEnd with no events") {
+        will("subscribe and then unsubscribe - expecting onEnd") {
+            var sub: Subscription by FutureValue()
+            sub = instance.subscribeNonResuming(
+                path = PATH_3_AND_OPEN,
+                retryOptions = RetryStrategyOptions(limit = 0),
+                listeners = listenersWithCounter(
+                    onEvent = {
+                        events++
+                        attempt { assertThat(events).isLessThan(4) }
+                        if (events == 3) sub.unsubscribe()
+                    },
+                    onEnd = { done() },
+                    onError = { done { error("onError triggered - this shouldn't be!") } }
+                )
+            )
+        }
+
+        will("subscribe and receive EOS immediately - expecting onEnd with no events") {
             instance.subscribeNonResuming(
                 path = PATH_0_EOS,
                 retryOptions = RetryStrategyOptions(limit = 0),
-                listeners = observeUntil(
-                    open = 1,
-                    end = 1
+                listeners = listenersWithCounter(
+                    onEvent = { fail("No events should have been received") },
+                    onEnd = { done() },
+                    onError = { fail("We should not error") }
                 )
             )
         }
 
-        will("subsccribes and receives EOS with retry-after headers") {
+        will("subscribe and receive EOS with retry-after headers") {
             instance.subscribeNonResuming(
                 path = "subscribe_retry_after",
                 retryOptions = RetryStrategyOptions(limit = 0),
-                listeners = observeUntil(
-                    error = 1
+                listeners = listenersWithCounter(
+                    onEvent = { fail("No events should have been receive") },
+                    onEnd = { fail("We should get an error") },
+                    onError = { error ->
+                        done {
+                            assertThat((error as ErrorResponse).headers)
+                                .containsEntry("Retry-After", listOf("10"))
+                        }
+                    }
                 )
             )
         }
--- library-core/src/integrationTest/kotlin/com/pusher/platform/test/SuspendTestBody.kt
+++ library-core/src/integrationTest/kotlin/com/pusher/platform/test/SuspendTestBody.kt
@@ -1,11 +1,11 @@
 package com.pusher.platform.test
 
-import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import org.jetbrains.spek.api.dsl.TestBody
 import org.jetbrains.spek.api.dsl.TestContainer
 import org.jetbrains.spek.api.dsl.it
-import java.util.concurrent.LinkedBlockingQueue
 
 /**
  * Spek extension to allow a test to block for threaded completion. It can be used inside a
@@ -17,41 +17,70 @@ import java.util.concurrent.LinkedBlockingQueue
  * as well as (and recommended to avoid endless tests) with the function `timeout {}` to provide
  * with an action if the test takes too long (default is 100ms).
  */
-class SuspendedTestBody : TestBody {
+class SuspendedTestBody(private val timeout: Long, body: SuspendedTestBody.() -> Unit) : TestBody {
 
-    private var isDone = false
-    private val queue = LinkedBlockingQueue<() -> Unit>()
+    private val conditions = mutableListOf<() -> SuspendTestResult>()
+    private val start = System.currentTimeMillis()
+    private val completeChannel = Channel<() -> Unit>()
 
-    /**
-     *
-     */
-    fun await() = queue.poll().invoke()
+    private fun duration() = System.currentTimeMillis() - start
+    private fun isTimedOut() = duration() >= timeout
+    private fun allConditionsPass() = conditions.all { it() === SuspendTestResult.Pass }
+    private fun collateConditionMessages() =
+        conditions.map { it() }
+            .mapIndexed { i, result ->
+                when (result) {
+                    is SuspendTestResult.Pass -> "$i: Pass"
+                    is SuspendTestResult.Failed -> "$i: Failed: \n${result.message}"
+                }
+            }
+            .joinToString("\n")
 
-    fun done(action: () -> Unit = {}) {
-        if (!isDone) queue.add(action)
-        isDone = true
+    init {
+        launch {
+            while (!isTimedOut() && !allConditionsPass());
+            done {
+                when {
+                    allConditionsPass() -> Unit
+                    else -> error("Timed out before matching all conditions: \n${collateConditionMessages()}")
+                }
+            }
+        }
+        launch {
+            body()
+        }
     }
 
-    /**
-     * Will trigger the provided action after [timeout] is the test is still running.
-     */
-    fun timeout(timeout: Int = 100, action: () -> Unit) = launch {
-        delay(timeout)
-        done(action)
+    fun until(condition: () -> SuspendTestResult) {
+        conditions += condition
     }
 
+    fun done(action: () -> Unit = {}) =
+        completeChannel.offer(action)
+
+    fun fail(cause: String) = done { error(cause) }
+
+    fun attempt(action: () -> Unit) {
+        try { action() }
+        catch (e: Throwable) { done { throw e } }
+    }
+
+    suspend fun await() =
+        completeChannel.receive().also {
+            completeChannel.close()
+        }
+
+}
+
+sealed class SuspendTestResult {
+    object Pass : SuspendTestResult()
+    data class Failed(val message: String) : SuspendTestResult()
 }
 
 /**
  * Entry point for [SuspendedTestBody]
  */
-fun TestContainer.will(description: String, timeout: Int = 100, body: SuspendedTestBody.() -> Unit) =
+fun TestContainer.will(description: String, timeout: Long = 5000, body: SuspendedTestBody.() -> Unit) =
     it("will $description") {
-        val testBody = SuspendedTestBody()
-        testBody.body()
-        testBody.await()
-        launch {
-            delay(timeout)
-            testBody.done { error("Timed out after: $timeout millis") }
-        }
+        runBlocking { SuspendedTestBody(timeout, body).await() }()
     }
