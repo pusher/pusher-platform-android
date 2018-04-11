fde9720845bd78cd8f6542bc896fe42697f3d1c9
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-11 12:53:11 +0100

    Formatting on RetryStrategyOptions

--- library-core/src/main/kotlin/com/pusher/platform/retrying/RetryStrategyOptions.kt
+++ library-core/src/main/kotlin/com/pusher/platform/retrying/RetryStrategyOptions.kt
@@ -1,3 +1,10 @@
 package com.pusher.platform.retrying
 
-data class RetryStrategyOptions(val initialTimeoutMillis: Long = 1000L, val maxTimeoutMillis: Long = 5000L, val limit: Int = -1)
+/**
+ * Describes options to be followed by the SDK when retrying failed requests.
+ */
+data class RetryStrategyOptions(
+    val initialTimeoutMillis: Long = 1000L,
+    val maxTimeoutMillis: Long = 5000L,
+    val limit: Int = -1
+)
