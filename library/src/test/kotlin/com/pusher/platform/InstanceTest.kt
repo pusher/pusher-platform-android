9a47a5eae8fe26f7995c260da804851c19faf681
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-12 23:24:03 +0100

    Add tests for composite listeners

--- library/src/test/kotlin/com/pusher/platform/InstanceTest.kt
+++ library/src/test/kotlin/com/pusher/platform/InstanceTest.kt
@@ -1,5 +1,6 @@
 package com.pusher.platform
 
+import com.google.common.truth.Truth
 import com.pusher.platform.test.SyncScheduler
 import mockitox.stub
 import org.junit.jupiter.api.Test
@@ -8,7 +9,7 @@ import kotlin.test.assertNotNull
 class InstanceTest {
 
     @Test
-    fun instanceSetUpCorrectly() {
+    fun `instance set up correctly`() {
         val instance = Instance(
             locator = "foo:bar:baz",
             serviceName = "bar",
@@ -16,6 +17,39 @@ class InstanceTest {
             dependencies = InstanceDependencies()
         )
         assertNotNull(instance)
+    }
+
+    @Test
+    fun `composition of multiple listeners`() {
+        var tracker = ""
+        val subscription = SubscriptionListeners.compose(
+            SubscriptionListeners(
+                onEnd = { tracker += "a" },
+                onOpen = { tracker += "b" },
+                onError = { tracker += "c" },
+                onEvent = { tracker += "d" },
+                onSubscribe = { tracker += "e" },
+                onRetrying = { tracker += "f" }
+            ),
+            SubscriptionListeners(
+                onEnd = { tracker += "aa" },
+                onOpen = { tracker += "bb" },
+                onError = { tracker += "cc" },
+                onEvent = { tracker += "dd" },
+                onSubscribe = { tracker += "ee" },
+                onRetrying = { tracker += "ff" }
+            )
+        )
+
+        subscription.onEnd(stub())
+        subscription.onOpen(stub())
+        subscription.onError(stub())
+        subscription.onEvent(stub())
+        subscription.onSubscribe()
+        subscription.onRetrying()
+
+        Truth.assertThat(tracker).isEqualTo("aaabbbcccdddeeefff")
+
     }
 
 }
